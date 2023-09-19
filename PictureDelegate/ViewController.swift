7237eccce0b654cbc68d9ab5b43e9060b7c092a0
Mirjigit <alaimirjigit154@gmail.com>
2023-09-19 20:36:07 +0600

    Initial Commit

---
+++ PictureDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureDelegate
+//
+//  Created by Миржигит Суранбаев on 19/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
